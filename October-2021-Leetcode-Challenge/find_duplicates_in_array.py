522a09a6ec05da049ab65f07d3a66eed785d2e00
smsrikanthreddy <smsrikanthreddy@gmail.com>
2021-10-07 16:57:17 +0530

    find dup in array

--- October-2021-Leetcode-Challenge/find_duplicates_in_array.py
+++ October-2021-Leetcode-Challenge/find_duplicates_in_array.py
@@ -1,6 +1,28 @@
+# O(N), O(N) - extra array is used
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
         output = {}
         for val in nums:
             output[val] = output.get(val, 0) + 1
         return sorted([k for (k, v) in output.items() if v == 2])
+
+## O(N), O(1)
+
+
+def printRepeating(arr, size):
+
+    print("The repeating elements are: ")
+
+    for i in range(0, size):
+
+        if arr[abs(arr[i])] >= 0:
+            arr[abs(arr[i])] = -arr[abs(arr[i])]
+        else:
+            print(abs(arr[i]), end=" ")
+
+
+# Driver code
+arr = [1, 2, 3, 1, 3, 6, 6]
+arr_size = len(arr)
+
+printRepeating(arr, arr_size)
