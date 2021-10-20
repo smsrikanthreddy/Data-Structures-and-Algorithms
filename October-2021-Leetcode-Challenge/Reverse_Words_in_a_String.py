d194372b801cbe644a8b68679e15fbd133ae2386
smsrikanthreddy <smsrikanthreddy@gmail.com>
2021-10-20 11:34:35 +0530

    reverse words in string

--- October-2021-Leetcode-Challenge/Reverse_Words_in_a_String.py
+++ October-2021-Leetcode-Challenge/Reverse_Words_in_a_String.py
@@ -1,11 +1,16 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        result = []
-        out = s.strip().split(" ")
-        for idx in range(len(out)-1, -1, -1):
-            if len(out[idx]) > 0:
-                result.append(out[idx])
-        return " ".join(w for w in result)
+        # result = []
+        # out = s.strip().split(" ")
+        # for idx in range(len(out)-1, -1, -1):
+        #     if len(out[idx]) > 0:
+        #         result.append(out[idx])
+        # return " ".join(w for w in result)
+
+        # return ' '.join(reversed(s.split()))
+
+        # joins each word in reversed list placing a space inbetween
+        return (' ').join(list(map(str, s.split()))[::-1])
 
 
 s = "  hello world  "
