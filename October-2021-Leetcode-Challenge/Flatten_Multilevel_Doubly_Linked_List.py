56e97eaa54198b05fa68fa7f7a1e4a840e998161
smsrikanthreddy <smsrikanthreddy@gmail.com>
2021-11-02 19:49:08 +0530

    flatten double linked list

---
+++ October-2021-Leetcode-Challenge/Flatten_Multilevel_Doubly_Linked_List.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+
+class Solution:
+    def flatten(self, head: 'Node') -> 'Node':
+        curr = head
+        while curr:
+            if curr.child is None:
+                curr = curr.next
+            else:
+                tail = curr.child
+                while tail.next:
+                    tail = tail.next
+                tail.next = curr.next
+                if curr.next:
+                    curr.next.prev = tail
+                curr.next = curr.child
+                curr.child.prev = curr
+                curr.child = None
+        return head
