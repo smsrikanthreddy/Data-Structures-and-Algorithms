8d526b4ac91dc42eac80d80b70615cd65b9d4fb1
smsrikanthreddy <smsrikanthreddy@gmail.com>
2021-11-20 07:07:22 +0530

    single element in a sorted array

---
+++ Nov-2021-Leetcode-Challenge/Single_Element_in_Sorted_Array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        n = len(nums)
+        idx = 0
+        while idx < n-1:
+            if nums[idx] == nums[idx+1]:
+                idx += 2
+            else:
+                return nums[idx]
+        return nums[idx]
